55252c5c9ba0d9c6ed3d24f2602523005263cf63
hotogi_shoya <hotogi.shoya@alhinc.jp>
2026-02-20 15:53:48 +0900

    first commit

---
+++ src/chapter6/beans/Message.java
@@ -0,0 +1,47 @@
+package chapter6.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Message implements Serializable {
+
+    private int id;
+    private int userId;
+    private String text;
+    private Date createdDate;
+    private Date updatedDate;
+
+
+    public int getId(){
+    	return id;
+    }
+    public int getUserId(){
+    	return userId;
+    }
+    public String getText(){
+    	return text;
+    }
+    public Date getCreatedDate(){
+    	return createdDate;
+    }
+    public Date getUpdatedDate(){
+    	return updatedDate;
+    }
+
+
+    public void setId(int id){
+    	this.id = id;
+    }
+    public void setUserId(int userId){
+    	this.userId = userId;
+    }
+    public void setText(String text){
+    	this.text = text;
+    }
+    public void setCreatedDate(Date createdDate){
+    	this.createdDate = createdDate;
+    }
+    public void setUpdatedDate(Date updatedDate){
+    	this.updatedDate = updatedDate;
+    }
+}
