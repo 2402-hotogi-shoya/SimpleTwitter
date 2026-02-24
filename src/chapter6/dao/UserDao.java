b362f9a7ab08c99599d7238127ba0c23895d5493
hotogi_shoya <hotogi.shoya@alhinc.jp>
2026-02-24 16:01:18 +0900

    簡易Twitter実践問題①

--- src/chapter6/dao/UserDao.java
+++ src/chapter6/dao/UserDao.java
@@ -170,7 +170,7 @@ public class UserDao {
         }
     }
 
-    public void update(Connection connection, User user) {
+    public void update(Connection connection, User user, boolean updatePassword) {
 
         log.info(new Object(){}.getClass().getEnclosingClass().getName() +
         " : " + new Object(){}.getClass().getEnclosingMethod().getName());
@@ -182,19 +182,29 @@ public class UserDao {
             sql.append("    account = ?, ");
             sql.append("    name = ?, ");
             sql.append("    email = ?, ");
-            sql.append("    password = ?, ");
+            if (updatePassword) {
+            	sql.append("    password = ?, ");
+            }
             sql.append("    description = ?, ");
             sql.append("    updated_date = CURRENT_TIMESTAMP ");
             sql.append("WHERE id = ?");
 
             ps = connection.prepareStatement(sql.toString());
 
-            ps.setString(1, user.getAccount());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getEmail());
-            ps.setString(4, user.getPassword());
-            ps.setString(5, user.getDescription());
-            ps.setInt(6, user.getId());
+            if (updatePassword) {
+            	ps.setString(1, user.getAccount());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getEmail());
+                ps.setString(4, user.getPassword());
+                ps.setString(5, user.getDescription());
+                ps.setInt(6, user.getId());
+            } else {
+            	ps.setString(1, user.getAccount());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getEmail());
+                ps.setString(4, user.getDescription());
+                ps.setInt(5, user.getId());
+            }
 
             int count = ps.executeUpdate();
             if (count == 0) {
