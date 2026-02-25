372d79ff86efb4f9f6ee51688d052fad711494ee
hotogi_shoya <hotogi.shoya@alhinc.jp>
2026-02-25 09:55:31 +0900

    isEmpty→isBlank

--- src/chapter6/dao/UserDao.java
+++ src/chapter6/dao/UserDao.java
@@ -184,7 +184,7 @@ public class UserDao {
             sql.append("    account = ?, ");
             sql.append("    name = ?, ");
             sql.append("    email = ?, ");
-            if (!StringUtils.isEmpty(user.getPassword())) {
+            if (!StringUtils.isBlank(user.getPassword())) {
             	sql.append("    password = ?, ");
             }
             sql.append("    description = ?, ");
@@ -197,7 +197,7 @@ public class UserDao {
             ps.setString(2, user.getName());
             ps.setString(3, user.getEmail());
 
-            if (!StringUtils.isEmpty(user.getPassword())) {
+            if (!StringUtils.isBlank(user.getPassword())) {
                 ps.setString(4, user.getPassword());
                 ps.setString(5, user.getDescription());
                 ps.setInt(6, user.getId());
--- src/chapter6/service/UserService.java
+++ src/chapter6/service/UserService.java
@@ -120,7 +120,7 @@ public class UserService {
         Connection connection = null;
         try {
             // パスワード暗号化
-        	if(!StringUtils.isEmpty(user.getPassword())) {
+        	if(!StringUtils.isBlank(user.getPassword())) {
         		String encPassword = CipherUtil.encrypt(user.getPassword());
         		user.setPassword(encPassword);
         	}
